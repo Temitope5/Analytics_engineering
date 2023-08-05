dd292af19d36029aefa6231a14c54e21bb96c585
Temitope <temitopefolajin@gmail.com>
2023-08-05 21:07:17 +0100

    Updated

--- Airbnb/dbtlearn/tests/consistent_created_at.sql
+++ Airbnb/dbtlearn/tests/consistent_created_at.sql
@@ -1,7 +1,8 @@
 SELECT *
 FROM
-{{ ref('dim_listings_cleansed') }}
-INNER JOIN {{ ref('fct_reviews') }} AS r
+{{ ref('dim_listings_cleansed') }} l
+INNER JOIN {{ ref('fct_reviews') }}  r
 USING (listing_id)
 WHERE l.created_at >= r.review_date
+
 
