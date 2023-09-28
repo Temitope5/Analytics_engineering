00d5f9e85141b376a673152e43bd2b1e928fa094
Temitope Olagoke <temitopeolagoke01@gmail.com>
2023-09-28 13:57:46 +0000

    commit

--- Jaffle_shop/models/staging/jaffle_shop/stg_orders.sql
+++ Jaffle_shop/models/staging/jaffle_shop/stg_orders.sql
@@ -1,14 +1,6 @@
-with 
-source as (
+with
+    source as (select * from {{ source("jaffle_shop", "orders") }}),
 
-    select * from {{ source('jaffle_shop','orders') }}
-),
-
-staged as (
-select id as order_id,
-user_id as customer_id,
-order_date,
-status
-from source
-)
-
+    staged as (
+        select id as order_id, user_id as customer_id, order_date, status from source
+    )
